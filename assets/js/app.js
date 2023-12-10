55459c65243d8b961b04990ff104f4868135556d
Andy Kirkpatrick <ubermonk@gmail.com>
2023-12-10 01:27:49 +0000

    JS alert on phx:alert message

--- assets/js/app.js
+++ assets/js/app.js
@@ -40,6 +40,7 @@ let liveSocket = new LiveSocket("/live", Socket, {hooks: Hooks, params: {_csrf_t
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
 window.addEventListener("phx:page-loading-start", _info => topbar.show(300))
 window.addEventListener("phx:page-loading-stop", _info => topbar.hide())
+window.addEventListener("phx:alert", function(e) { alert(e.detail.message) })
 
 // connect if there are any LiveViews on the page
 liveSocket.connect()
